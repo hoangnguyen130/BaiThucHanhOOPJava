f7e20db80f97c99b11d52b71cf4379c16b59c490
hoangnguyen130 <hoangnguyen130520@gmail.com>
2022-01-17 16:29:56 +0700

    BaiThucHanhSo2

--- HocJavaOOP/BaiThucHanhSo2/hoten.java
+++ HocJavaOOP/BaiThucHanhSo2/hoten.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class App {
+public class hoten {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         String hoten ="";
