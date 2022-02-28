1a0cd9bbe929ae834d02ff286c9cd8c7d22d25b8
hoangnguyen130 <hoangnguyen130520@gmail.com>
2022-02-28 19:49:44 +0700

    NguyenHuyHoang2021050857

--- HocJavaOOP/BaiThucHanh2802/Bai11.java
+++ HocJavaOOP/BaiThucHanh2802/Bai11.java
@@ -5,8 +5,8 @@ public class Bai11 {
         for (i = 0; i <= 200; ++i) {
             for (j = 0; j <= 100; ++j) {
                 for (k = 0; k <= 40; ++k) {
-                    if (i * 1000 + j * 2000 + k * 5000 == 200000) {
-                        System.out.printf("\nCan %d to 100k, va %d to 200k, va %d to 500k", i, j, k);
+                    if (i * 10000 + j * 20000 + k * 50000 == 200000) {
+                        System.out.printf("\nCan %d to 10k, va %d to 20k, va %d to 50k", i, j, k);
                     }
                 }
             }
